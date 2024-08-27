34b0103627396089a286de34e9929130e785a4af
LucasRossatto <154370022+LucasRossatto@users.noreply.github.com>
2024-08-27 14:07:33 -0300

    add cores e footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,43 @@
 import "../style/footer.css"
 
-const Footer = ({titulo, desc}) =>{
+const Footer = ({titulo, desc, ano, nomeEmpresa}) =>{
 
 return(
     <div className="footer-container">
+      
+<div className="text-container">
         <div className="text-controller">
             <h1>{titulo}</h1>
             <h2>{desc}</h2>
         </div>
+
+        <div className="text-controller">
+            <h1>Ajuda</h1>
+            <h2>texto</h2>
+            <h2>texto</h2>
+        </div>
+
+        <div className="text-controller">
+            <h1>Sobre nós</h1>
+            <h2>texto</h2>
+            <h2>texto</h2>
+        </div>
+
+        <div className="text-controller">
+            <h1>Redes Socias</h1>
+            <h2>texto</h2>
+            <h2>texto</h2>
+        </div>
     </div>
-    
+        <hr />
+
+        <div className="text-container">
+                <h2>politicas de privacidade</h2>
+                <h2>termos de uso</h2>
+                <h2>© {ano} por {nomeEmpresa}. Todos os direitos reservados. </h2>
+        </div>
+    </div>
+
 )
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,8 @@ createRoot(document.getElementById('root')).render(
     < Footer
     titulo={"Texto"}
     desc={"lorem"}
+    ano={"2024"}
+    nomeEmpresa={"Corretora BlackDiamond"}
     />
   </StrictMode>,
 )
